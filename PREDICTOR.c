c11dd410a54eb1412c114fa19210db29b8660159
siddharthiitian <98660934+siddharthiitian@users.noreply.github.com>
2022-03-03 14:35:57 +0530

    Add files via upload

---
+++ PREDICTOR.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+
+{
+    int D,Y,C,M;
+    printf("enter the day /n");
+
+    scanf("%d",&D);
+
+    printf("enter the month in digits /n");
+    scanf("%d",&M);
+
+    printf("enter the century like if it is 2002 then 20 if it is 1987 then 19 /n");
+    scanf("%d",&C);
+
+
+    printf("enter the year like if it is 1987 then enter 87 except if then month is jan or feb enter 1986");
+    scanf("%d",&Y);
+
+    
+       
+    
+       return 0;
+}
